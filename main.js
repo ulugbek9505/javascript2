3696593c1087c6841b79a295facb9db74e0e2671
ulugbek9505 <mtvazirligi@gmail.com>
2024-12-14 00:59:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let promp = Number(prompt("Ogan balli kiritasan keyin javobini korasa"));
+let maxBall = 65;
+
+if (promp >= maxBall) {
+    console.log("Tabriklayman san otin");
+} else if (promp >= 1){
+    console.log("Otomadin");
+} else {
+   console.log("Qaytadan kirit");
+}
